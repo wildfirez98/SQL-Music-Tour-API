1ccb1779c30b43b3158c701c70823b2535eea703
wildfirez98 <james@areafamily.us>
2022-04-23 13:30:30 -0500

    Added a sort order and like operator in Index route

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,4 +1,5 @@
 // DEPENDENCIES
+const { Op } = require('sequelize')
 
 // REMEMBER - Router comes from Express Framework for Node.js
 const bands = require('express').Router() // Create the router and save to variable named bands 
@@ -11,7 +12,12 @@ const { Band } = db // Access database models thru db variable. Example, db.Band
 // Route 1: Index "/" - GET
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC' ] ],
+            where: {
+                name: { [ Op.like ]: `%${req.query.name ? req.query.name : ''}%`}
+            }
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
